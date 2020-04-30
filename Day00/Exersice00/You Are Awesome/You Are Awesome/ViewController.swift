4ff22fe3330e194eb76d205806e83af6732d5582
PurpleRiver <ktomogetkak@mail.ru>
2020-04-30 20:09:54 +0300

    exercise00

---
+++ Day00/Exersice00/You Are Awesome/You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Blurry Rabbit on 30.04.2020.
+//  Copyright © 2020 Blurry Rabbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
